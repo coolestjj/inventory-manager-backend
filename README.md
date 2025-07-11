7fab8b5fed8702494376d29a8bb2bbe99e34015c
Jiajun Li <1009839598@qq.com>
2025-07-11 01:58:38 -0500

    Swagger added, README.md added

---
+++ README.md
@@ -0,0 +1,15 @@
+# Inventory-manager-backend
+
+## Tech Stack:
+Java, Spring Boot, Spring Data JPA, MySQL, Maven, Swagger
+
+## Features:
+
+1. Donation Registration: A feature that allows the shelter staff to record details of the donations, such as the donor's name, type of donation (money, food, clothing, etc.), quantity or amount donated, and the date of the donation.  
+2. Donation Distribution: A feature to log when and how much of the donations are distributed, capturing the type of donation, quantity or amount distributed, and the date of distribution.  
+3. Donation Reports: Your solution should have the capacity to generate two types of reports: (1) An inventory report displaying the current status of donations, grouped by type. (2) A donator report, summarizing the total contributions received from each donor.  
+
+## How to use:
+
+1. Download the repository, make sure you have Java environment set up, and run the application using Maven.
+2. Visit `http://localhost:8080/swagger-ui.html` to access the Swagger UI for API documentation and testing.
--- src/main/java/com/example/inventorymanagerbackend/controller/DonationController.java
+++ src/main/java/com/example/inventorymanagerbackend/controller/DonationController.java
@@ -3,6 +3,7 @@ package com.example.inventorymanagerbackend.controller;
 import com.example.inventorymanagerbackend.entity.Distribution;
 import com.example.inventorymanagerbackend.entity.Donation;
 import com.example.inventorymanagerbackend.service.DonationService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -16,21 +17,25 @@ public class DonationController {
         this.donationService = donationService;
     }
 
+    @Operation(summary = "Add a new donation")
     @PostMapping("/donate")
     public void addDonation(@RequestBody Donation donation) {
         donationService.saveDonation(donation);
     }
 
+    @Operation(summary = "Add a new distribution")
     @PostMapping("/distribute")
     public void addDistribution(@RequestBody Distribution distribution) {
         donationService.saveDistribution(distribution);
     }
 
+    @Operation(summary = "Get inventory report (Total Amount for each type)")
     @GetMapping("/reports/inventory")
     public Map<String, Integer> getInventoryReport() {
         return donationService.getInventoryReport();
     }
 
+    @Operation(summary = "Get donor report (By person and type)")
     @GetMapping("/reports/donors")
     public Map<String, Map<String, Integer>> getDonorsReport() {
         return donationService.getDonorReport();
